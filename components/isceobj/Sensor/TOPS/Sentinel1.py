96036994c089db00aa84de07613e334aea211fe3
shitong01 <stchin@ntu.edu.sg>
2026-06-22 11:24:10 +0800

    Sentinel-1C and 1D updates

--- components/isceobj/Sensor/TOPS/Sentinel1.py
+++ components/isceobj/Sensor/TOPS/Sentinel1.py
@@ -604,7 +604,13 @@ class Sentinel1(Component):
             elif mission == 'S1B':
                 burst.trackNumber = (orbitnumber-27)%175 + 1
             elif mission == 'S1C':
-                burst.trackNumber = (orbitnumber-172)%175 + 1
+                S1C_EPOCH = datetime.datetime(2026, 6, 24)
+                if ascTime < S1C_EPOCH:
+                    burst.trackNumber = (orbitnumber-172)%175 + 1
+                else:
+                    burst.trackNumber = (orbitnumber-99)%175 + 1
+            elif mission == 'S1D':
+                burst.trackNumber = (orbitnumber-42)%175 + 1
             else:
                 raise ValueError('Encountered unknown mission id {0}'.format(mission))
 
